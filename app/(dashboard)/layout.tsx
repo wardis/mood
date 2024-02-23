3fd7f82ed82ec24ed4b8744ec2b8436756782726
Wardi Soulaimana <wardi@wardi-s.com>
2024-02-23 18:10:52 +0000

    nav link

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,11 +1,24 @@
 import { UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
 import { PropsWithChildren } from 'react'
+
+const links = [
+  { href: '/', label: 'Home' },
+  { href: '/journal', label: 'Journal' },
+]
 
 export default function DashboardLayout({ children }: PropsWithChildren) {
   return (
     <div className="relative h-screen w-screen">
       <aside className="absolute left-0 top-0 h-full w-[200px] border-r border-black/10">
-        Mood
+        <div>Mood</div>
+        <ul>
+          {links.map((link) => (
+            <li key={link.href} className="px-2 py-6 text-xl">
+              <Link href={link.href}>{link.label}</Link>
+            </li>
+          ))}
+        </ul>
       </aside>
       <div className="ml-[200px] h-full">
         <header className="h-[60px] border-b border-black/10">

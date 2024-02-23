117f8a14aaaf7a516194853d9779369e238e7b7a
Wardi Soulaimana <wardi@wardi-s.com>
2024-02-23 16:02:48 +0000

    add clerk authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { UserButton } from '@clerk/nextjs'
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
             </button>
           </Link>
         </div>
+        <UserButton />
       </div>
     </div>
   )

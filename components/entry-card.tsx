07421c76a72e07eeb589823a0507b29ad4848a5c
Wardi Soulaimana <wardi@wardi-s.com>
2024-02-23 16:02:49 +0000

    add journal entries list

--- components/entry-card.tsx
+++ components/entry-card.tsx
@@ -1,3 +1,12 @@
 export default function EntryCard({ entry }) {
-  return <div>Entry Card {entry.id}</div>
+  const date = new Date(entry.createdAt).toDateString()
+  return (
+    <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
+      <div className="px-4 py-5 sm:px-6">{date}</div>
+      <div className="px-4 py-5 sm:p-6">
+        {entry.analysis?.summary || 'summary'}
+      </div>
+      <div className="px-4 py-4 sm:px-6">{entry.analysis?.mood || 'mood'}</div>
+    </div>
+  )
 }

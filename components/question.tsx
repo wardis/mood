bc3224b36e06ae302dccc75132b18dc9d283c163
Wardi Soulaimana <wardi@wardi-s.com>
2024-02-23 19:48:39 +0000

    link qa search component

--- components/question.tsx
+++ components/question.tsx
@@ -1,23 +1,33 @@
 'use client'
 
+import { askQuestion } from '@/utils/api'
 import { useState } from 'react'
 
 export default function Question() {
   const [value, setValue] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [response, setResponse] = useState('')
+
   const onChange = (e) => {
     setValue(e.target.value)
   }
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
+    setLoading(true)
 
-    // do stuff
+    const answer = await askQuestion(value)
+    setResponse(answer)
+
+    setValue('')
+    setLoading(false)
   }
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <input
+          disabled={loading}
           value={value}
           onChange={onChange}
           type="text"
@@ -25,12 +35,15 @@ export default function Question() {
           className="mr-2 rounded-lg border border-black/20 px-4 py-2 text-lg"
         />
         <button
+          disabled={loading}
           type="submit"
           className="rounded-lg bg-blue-400 px-4 py-2 text-lg"
         >
           Ask
         </button>
       </form>
+      {loading && <div>...loading</div>}
+      {response && <div>{response}</div>}
     </div>
   )
 }

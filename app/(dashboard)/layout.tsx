c6d204ee976f1ec6d2706025ebb1d499277cf9b0
Wardi Soulaimana <wardi@wardi-s.com>
2024-02-23 19:28:09 +0000

    create similary search function

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -11,7 +11,7 @@ export default function DashboardLayout({ children }: PropsWithChildren) {
   return (
     <div className="relative h-screen w-screen">
       <aside className="absolute left-0 top-0 h-full w-[200px] border-r border-black/10">
-        <div>Mood</div>
+        <div className="px-2 py-4 text-4xl">Mood</div>
         <ul>
           {links.map((link) => (
             <li key={link.href} className="px-2 py-6 text-xl">
--- utils/ai.ts
+++ utils/ai.ts
@@ -1,6 +1,10 @@
+import { loadQARefineChain } from 'langchain/chains'
+import { Document } from 'langchain/document'
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
 import { OpenAI } from 'langchain/llms/openai'
 import { StructuredOutputParser } from 'langchain/output_parsers'
 import { PromptTemplate } from 'langchain/prompts'
+import { MemoryVectorStore } from 'langchain/vectorstores/memory'
 import z from 'zod'
 
 const parser = StructuredOutputParser.fromZodSchema(
@@ -51,4 +55,26 @@ export const analyse = async (content) => {
   } catch (e) {
     console.log(e)
   }
+}
+
+export const qa = async (question, entries) => {
+  const docs = entries.map((entry) => {
+    return new Document({
+      pageContent: entry.content,
+      metadata: { id: entry.id, createdAt: entry.createdAt },
+    })
+  })
+
+  const model = new OpenAI({ temperature: 0, modelName: 'gpt-3.5-turbo' })
+  const chain = loadQARefineChain(model)
+  const embeddings = new OpenAIEmbeddings()
+  const store = await MemoryVectorStore.fromDocuments(docs, embeddings)
+  const relevantDocs = await store.similaritySearch(question)
+
+  const res = await chain.call({
+    input_documents: relevantDocs,
+    question,
+  })
+
+  return res.output_text
 }

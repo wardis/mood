e6d89ff08470be749c43513a8a1b03b2be8a8e9d
Wardi Soulaimana <wardi@wardi-s.com>
2024-02-23 16:02:49 +0000

    add autosaving for entries

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,16 +1,29 @@
 'use client'
 
+import { updateEntry } from '@/utils/api'
 import { useState } from 'react'
+import { useAutosave } from 'react-autosave'
 
 export default function Editor({ entry }) {
   const [value, setValue] = useState(entry.content)
+  const [isLoading, setIsLoading] = useState(false)
+
+  useAutosave({
+    data: value,
+    onSave: async (_value) => {
+      setIsLoading(true)
+      const updated = await updateEntry(entry.id, _value)
+      setIsLoading(false)
+    },
+  })
 
   return (
     <div className="h-full w-full overflow-hidden">
+      {isLoading && <div>...loading </div>}
       <textarea
+        className="h-full w-full p-8 text-xl outline-none"
         value={value}
         onChange={(e) => setValue(e.target.value)}
-        className="h-full w-full p-8 text-xl outline-none"
       />
     </div>
   )
